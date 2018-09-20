fedc6fdc87db750024e543b2264c1458e4a41752
James Stafford <James@Jamess-Air.hitronhub.home>
2018-09-20 12:31:01 -0700

    greet new users and alert others

--- server/server.js
+++ server/server.js
@@ -17,15 +17,36 @@ var io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('new user connected');
 
+  // Welcomes new user as soon as they connect
+  socket.emit('newMessage', {
+    from: "Admin",
+    text: "Welcome to the chat app",
+    createdAt: new Date().getTime()
+  });
+
+  // Broadcast to everyone that a new user has joined the chat room
+  socket.broadcast.emit('newMessage', {
+    from: "Admin",
+    text: "New User Joined",
+    createdAt: new Date().getTime()
+  })
+
+
+
   // listens for a created message
   socket.on('createMessage', (message) => {
     console.log('created message', message);
     // emits event to all connections
-    io.emit('newMessage', {
+    /*io.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
-    });
+    });*/
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
