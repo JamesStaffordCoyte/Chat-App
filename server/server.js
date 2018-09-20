07b9f3bbb5f5025b5fbfee1fc5ab5d601167e9f1
James Stafford <James@Jamess-Air.hitronhub.home>
2018-09-20 09:46:33 -0700

    first commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+/* Root Node application */
+
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '/..', '/public');
+const port = process.env.PORT || 3000
+/* Create express app */
+var app = express();
+
+/* Configure the public directory */
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+  console.log(`server is up on ${port}`);
+});
